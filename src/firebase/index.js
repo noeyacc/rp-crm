c678dc3007d5e8a07c77564ab13b263b4b1b7247
DESKTOP-A1NFIVV\noeyire <muzakdaum@gmail.com>
2020-11-09 18:09:49 +0800

    remove firebase plugin

--- src/firebase/index.js
+++
@@ -1,8 +0,0 @@
-import firebase from "firebase/app";
-import { firebaseConfig } from "./dbConfig";
-import "firebase/firestore";
-
-// Initialize firebase app
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
